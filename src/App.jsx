ad392101cd09d43a360ce8034edf5f621602cdd9
jungho-Kang <rkdwjdgh08@gmail.com>
2025-01-13 12:32:37 +0900

    [docs] Ant Design 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
+import JoinForm from "./components/JoinForm";
+
 function App() {
-  return (
-    <div className="bg-brand text-brand-light p-72">
-      Custom Colors and Spacing
-    </div>
-  );
+  return <JoinForm />;
 }
 export default App;
--- src/components/JoinForm.jsx
+++ src/components/JoinForm.jsx
@@ -1,21 +1,3 @@
-# Ant Design
-
-- 알리바바
-- UI css 수정이 엄청 어렵다
-- 각 컴포넌트 사용법 학습에 시간이 오래 걸린다
-- 틈나는 대로 하나씩 공부하여 보자
-- UI 코딩 및 css 작업 시간 단축 및 통일성 있는 UI 구성 가능
-- https://ant.design/components/overview/
-
-## 설치
-
-- `npm install antd --save`
-
-## 실습
-
-- `/src/components/JoinForm.jsx`
-
-```jsx
 import { Button, Form, Input } from "antd";
 
 const JoinForm = () => {
@@ -102,4 +84,3 @@ const JoinForm = () => {
 };
 
 export default JoinForm;
-```
